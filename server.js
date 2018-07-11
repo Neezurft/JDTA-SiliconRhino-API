ed6a2fd08b5dec5e5ffecd41537667442ae1ee18
Alberto <albertjmrs@gmail.com>
2018-07-11 12:22:32 +0100

    Allow Origin header added

--- server.js
+++ server.js
@@ -7,7 +7,23 @@ const app            = express();
 
 const port = process.env.PORT || 8000;
 
-app.use(bodyParser.urlencoded({ extended: true })); 
+app
+.use( bodyParser.urlencoded({ extended: true }) )
+.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://embeddednotes.com');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  //res.setHeader('Access-Control-Allow-Credentials', true);
+});
 
 MongoClient.connect(datab.url, { useNewUrlParser: true }, (err, database) => {
     if (err) {
